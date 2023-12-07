6ed9f17b0667e654a5fbe2acb15057c3fc510a54
Andrey <gormilkoandrey@gmail.com>
2023-12-07 19:48:33 +0200

    First commit

---
+++ weather.py
@@ -0,0 +1,34 @@
+import telebot
+from telebot import types
+import requests
+import json
+
+bot = telebot.TeleBot('6570604194:AAGhEyZpIvDXheMyHCGbY69qc2mRZUgvAM0')
+API = '24d64e7a107896ae0ce4831920542be7'
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    bot.send_message(msg.chat.id, 'Привет, напиши название города')
+
+@bot.message_handler(content_types=['text'])
+def get_weather(msg):
+    city = msg.text.strip().lower()
+    req = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if req.status_code == 200:
+        data = json.loads(req.text)
+        temp = data["main"]["temp"]
+        weather = data["weather"][0]["main"]
+        bot.reply_to(msg, f'Температура у указанном городе: {temp} °C\nПогода: \n')
+        if weather.lower() == 'clouds':
+            file = open('img/cloud.bmp', 'rb')
+            bot.send_photo(msg.chat.id, file)
+        if weather.lower() == 'rain':
+            file = open('img/rain.bmp', 'rb')
+            bot.send_photo(msg.chat.id, file)
+        if weather.lower() == 'clear':
+            file = open('img/clear.bmp', 'rb')
+            bot.send_photo(msg.chat.id, file)
+    else:
+        bot.reply_to(msg, f'Город не найден')
+
+bot.polling(non_stop=True)
